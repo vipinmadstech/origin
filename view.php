1941c78411729d4d924d37edefbc0358362f4161
vipinmadstech <vipin.singh@madstech.com>
2019-12-10 10:47:55 +0530

    new branch testing

--- view.php
+++ view.php
@@ -1,3 +1,7 @@
 <?php
+echo "ok";
 echo phpinfo();
+public function getAllUpdateList($versionCode,$plateform) {
+	$select->where(new \Zend\Db\Sql\Predicate\Expression("t1.version_code>$versionCode"));
+	$this->_versioncode = $tokenResponse['versioncode'];
 ?>
